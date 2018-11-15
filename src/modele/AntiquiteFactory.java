a04ddc89b5da7d5c0d379648679f7943657268a8
levy54u <damienlevy546@gmail.com>
2018-11-15 15:18:52 +0100

    factory

--- src/modele/AntiquiteFactory.java
+++ src/modele/AntiquiteFactory.java
@@ -3,6 +3,8 @@
  */
 package modele;
 
+import modele.bateau.Bateau;
+
 /**
  * @author levy54u
  *
@@ -14,6 +16,36 @@ public class AntiquiteFactory implements EpoqueFactory {
 	 */
 	public AntiquiteFactory() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Bateau createBateau2() {
+		
+		return null;
+	}
+
+	@Override
+	public Bateau createBateau3_1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bateau createBateau3_2() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bateau createBateau4() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bateau createBateau5() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/modele/EpoqueFactory.java
+++ src/modele/EpoqueFactory.java
@@ -3,15 +3,17 @@
  */
 package modele;
 
+import modele.bateau.Bateau;
+
 /**
  * @author levy54u
  *
  */
 public interface EpoqueFactory {
 	
-	Bateau createBateau2();
-	Bateau createBateau3_1();
-	Bateau createBateau3_2();
-	Bateau createBateau4();
-	Bateau createBateau5();
+	public Bateau createBateau2();
+	public Bateau createBateau3_1();
+	public Bateau createBateau3_2();
+	public Bateau createBateau4();
+	public Bateau createBateau5();
 }
